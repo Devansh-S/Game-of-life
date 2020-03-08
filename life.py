a5db90a86a388e190fd13b5ed1a18a5625053cf3
Devansh Singh <42682284+Devansh-S@users.noreply.github.com>
2020-03-09 00:46:16 +0530

    Update life.py

--- life.py
+++ life.py
@@ -7,108 +7,117 @@
 import numpy as np
 import random
 import turtle
+import argparse
 
 
-class draw_game():
-    def __init__(self, size):
-        self.size = size
- 
-        scr=turtle.Screen()
-        turtle.setup(1000,1000)
-        turtle.title("Game of Life")
-        turtle.hideturtle()
-        turtle.speed(0)
-        turtle.tracer(0,0)
+scr=turtle.Screen()
+turtle.setup(500,500)
+turtle.title("Game of Life")
+turtle.hideturtle()
+turtle.speed(0)
+turtle.tracer(0,0)
 
-        lifeturtle = turtle.Turtle()
-        lifeturtle.up()
-        lifeturtle.hideturtle()
-        lifeturtle.speed(0)
-        lifeturtle.color('black')
-
-    def draw_line(x1,y1,x2,y2):
+lifeturtle = turtle.Turtle()
+lifeturtle.up()
+lifeturtle.hideturtle()
+lifeturtle.speed(0)
+lifeturtle.color('black')
+    
+def draw_grid(size):
+    turtle.pencolor('grey')
+    turtle.pensize(2)
+    x = -200
+    for i in range(size+1):
         turtle.up()
-        turtle.goto(x1,y1)
+        turtle.goto(x,-200)
         turtle.down()
-        turtle.goto(x2,y2)
-        
-    def draw_grid(size): 
-        turtle.pencolor('gray')
-        turtle.pensize(3)
-        x = -400
-        for i in range(self.size+1):
-            draw_line(x,-400,x,400)
-            x += 800/self.size
-        y = -400
-        for i in range(self.size+1):
-            draw_line(-400,y,400,y)
-            y += 800/size
+        turtle.goto(x,200)
+        x += 400/size
+    y = -200
+    for i in range(size+1):
+        turtle.up()
+        turtle.goto(-200,y)
+        turtle.down()
+        turtle.goto(200,y)
+        y += 400/size
 
-    def square(x,y,size_block):
-        lifeturtle.up()
-        lifeturtle.goto(x,y)
-        lifeturtle.down()
-        lifeturtle.seth(0)
-        lifeturtle.begin_fill()
-        for i in range(4):
-            lifeturtle.fd(size_block)
-            lifeturtle.left(90)
-        lifeturtle.end_fill()
+def square(x, y, size_block):
+    lifeturtle.up()
+    lifeturtle.goto(x,y)
+    lifeturtle.down()
+    lifeturtle.seth(0)
+    lifeturtle.begin_fill()
+    for i in range(4):
+        lifeturtle.fd(size_block)
+        lifeturtle.left(90)
+    lifeturtle.end_fill()
 
-    def draw_species(size):
-        global board
-        for i in range(self.size):
-            for j in range(self.size):
-                if board[i][j] == 1:
-                    lx = 800/self.size*i - 400
-                    ly = 800/self.size*j - 400
-                    square(lx+1,ly+1,800/self.size-2)
-                    
-    def draw_board(x_size, y_size):
-        board = np.zeros((x_size, y_size))
-        for x in range(x_size):
-            for y in range(y_size):
-                board[x][y] = int(random.randrange(2));
-        return board
+def draw_species(size):
+    for i in range(size):
+        for j in range(size):
+            if board[i][j] == 1:
+                px = 400/size*i - 200
+                py = 400/size*j - 200
+                square(px+1,py+1,400/size-2)
 
            
-    def update_life(size): 
-        global board 
-        neighbors = 0
-        board1 = draw_board(self.size,self.size)
-        for x in range(0,self.size):
-            for y in range(0,self.size):
-                neighbors = 0
-                neighbors = int((board[x, (y-1)%self.size] +
-                             board[x, (y+1)%self.size] + 
-                             board[(x-1)%self.size, y] +
-                             board[(x+1)%self.size, y] + 
-                             board[(x-1)%self.size, (y-1)%self.size] +
-                             board[(x-1)%self.size, (y+1)%self.size] + 
-                             board[(x+1)%self.size, (y-1)%self.size] +
-                             board[(x+1)%self.size, (y+1)%self.size]))
+def update_life(size):
+
+    neighbors = 0
+    for x in range(0,size):
+        for y in range(0,size):
+            neighbors = 0
+            neighbors = int((board[x, (y-1)%size] +
+                         board[x, (y+1)%size] + 
+                         board[(x-1)%size, y] +
+                         board[(x+1)%size, y] + 
+                         board[(x-1)%size, (y-1)%size] +
+                         board[(x-1)%size, (y+1)%size] + 
+                         board[(x+1)%size, (y-1)%size] +
+                         board[(x+1)%size, (y+1)%size]))
                 
-                if ((board[x][y] == 1) and (neighbors <  2)):
-                    board1[x][y] = 0
-                elif ((board[x][y] == 1) and (neighbors >  3)):
-                    board1[x][y] = 0           
-                elif ((board[x][y] == 0) and (neighbors == 3)):
-                    board1[x][y] = 1
-                else:
-                    board1[x][y] = board[x][y]
+            if ((board[x][y] == 1) and (neighbors <  2)):
+                temp_board[x][y] = 0
+            elif ((board[x][y] == 1) and (neighbors >  3)):
+                temp_board[x][y] = 0           
+            elif ((board[x][y] == 0) and (neighbors == 3)):
+                temp_board[x][y] = 1
+            else:
+                temp_board[x][y] = board[x][y]
 
-        board[:] = board1[:]
+    board[:] = temp_board[:]
+
+def main(refresh_rate=200, size=40):
+
+    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.") 
+  
+    parser.add_argument('--board_size', dest='size', required=False) 
+    parser.add_argument('--refresh_rate', dest='refresh_rate', required=False) 
+
+    args = parser.parse_args() 
+      
+    size = 50
+    if args.size and int(args.size) > 8: 
+        size = int(args.size)
+
+    global board
+    global temp_board
+
+    temp_board = np.random.randint(2, size=(size,size))
+    board = np.random.randint(2, size=(size,size))
+      
+    refresh_rate = 200
+    if args.refresh_rate: 
+        refresh_rate = int(args.refresh_rate)  
+
+    while(True):
+        draw_grid(size)
+        update_life(size)
         lifeturtle.clear()
-        draw_species(self.size)
+        draw_species(size)
         scr.update() 
-        scr.ontimer(update_life(self.size),200) 
+        scr.ontimer(update_life(size),refresh_rate) 
 
 
-    def game(size):
-        init()
-        global board   
-        board = draw_board(self.size,self.size)
-        while(True):
-            draw_grid(self.size)
-            update_life(self.size)
-
+if __name__ == '__main__': 
+    main()
